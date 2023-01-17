ac0132d3ba3ff11500245f0b2f34a686da775c5e
Abubakar Jilani <abub1156@gmail.com>
2023-01-17 19:18:30 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Join from "./component/join/Join";
+import Chat from "./component/chat/Chat";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Join />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/chat/Chat.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import { user } from "../join/Join";
+import socketIo from "socket.io-client";
+import "./chat.css";
+import sendLogo from "../../images/send.png";
+import ReactScrollToBottom from "react-scroll-to-bottom";
+import closeIcon from "../../images/closeIcon.png";
+import Message from "../message/Message";
+const ENDPOINT = "https://server-chat-app.vercel.app/";
+// const ENDPOINT = "http://localhost:5000/";
+let socket;
+const Chat = () => {
+  const [id, setid] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket = socketIo(ENDPOINT, {
+      cors: {
+        origin: "*",
+        credentials: true,
+      },
+      transports: ["websocket", "polling"],
+    });
+    console.log("socket", socket);
+
+    socket.emit("joined", { user });
+    socket.on("welcome", (data) => {
+      setMessages([...messages, data]);
+      setid(socket.id);
+    });
+    socket.on("userJoined", (data) => {
+      setMessages([...messages, data]);
+    });
+    socket.on("leave", (data) => {
+      setMessages([...messages, data]);
+    });
+
+    return () => {
+      socket.disconnect();
+      socket.off();
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.on("sendMessage", (data) => {
+      setMessages([...messages, data]);
+      //   console.log(data.user + data.message);
+    });
+    return () => {
+      socket.off();
+    };
+  }, [messages]);
+
+  const send = () => {
+    const message = document.getElementById("chatInput").value;
+    socket.emit("message", { message, id });
+    document.getElementById("chatInput").value = "";
+  };
+  return (
+    <div className="chatPage">
+      <div className="chatContainer">
+        <div className="header">
+          <h2>C CHAT</h2>
+          <a href="/">
+            {" "}
+            <img src={closeIcon} alt="Close" />
+          </a>
+        </div>
+        <ReactScrollToBottom className="chatBox">
+          {messages.map((item, i) => (
+            <Message
+              user={item.id === id ? "" : item.user}
+              message={item.message}
+              classs={item.id === id ? "right" : "left"}
+            />
+          ))}
+        </ReactScrollToBottom>
+        <div className="inputBox">
+          <input
+            onKeyPress={(event) => (event.key === "Enter" ? send() : null)}
+            type="text"
+            id="chatInput"
+          />
+          <button onClick={send} className="sendBtn">
+            <img src={sendLogo} alt="Send" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
